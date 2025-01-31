9145d630af72b169b053fa32c071212c3aaf724f
yhoung <mysterokhidjhnr020@gmail.com>
2025-01-31 09:20:45 +0000

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,32 +1,5 @@
-import type { Metadata } from "next";
-import { Inter, DM_Sans } from "next/font/google";
+
 import "./globals.css";
-
-const inter = Inter({ subsets: ["latin"] });
-const Sans = DM_Sans({
-  subsets: ["latin"],
-  weight: ["400", "500", "700"],
-  variable: "--font-dm-sans",
-});
-
-export const metadata: Metadata = {
-  title: "Esug E-learning Platform",
-  description: "A platform for Engineering Students in the University of Ghana",
-  icons: {
-    icon: [
-      {
-        media: "(prefers-color-scheme: light)",
-        url: "/android-chrome-192x192.png",
-        href: "/android-chrome-192x192.png",
-      },
-      {
-        media: "(prefers-color-scheme: dark)",
-        url: "/android-chrome-192x192.png",
-        href: "/android-chrome-192x192.png",
-      },
-    ],
-  },
-};
 
 export default function RootLayout({
   children,
@@ -36,7 +9,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${Sans.className} "max-w-screen overflow-clip" `}
+        className={`"max-w-screen overflow-clip" `}
       >
           {children}
       </body>
